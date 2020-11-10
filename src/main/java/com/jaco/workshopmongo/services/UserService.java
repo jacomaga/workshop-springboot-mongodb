e15bebee0c3a23d2960f8437977e1a7a1d185eda
Jacó <jacomagalhaes@gmail.com>
2020-11-10 14:56:43 -0300

    Buscando usuário por ID e tratando exceções no corpo do request

--- src/main/java/com/jaco/workshopmongo/services/UserService.java
+++ src/main/java/com/jaco/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.jaco.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jaco.workshopmongo.domain.User;
 import com.jaco.workshopmongo.repository.UserRepository;
+import com.jaco.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,9 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> object = repo.findById(id);
+		return object.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
